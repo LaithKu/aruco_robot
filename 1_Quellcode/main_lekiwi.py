9e1bbf81cbdfe45a31bd61b2ef1bc5a39242822e
LaithKu <119522324+LaithKu@users.noreply.github.com>
2026-09-11 17:10:38 +0200

    Update main_lekiwi.py

--- 1_Quellcode/main_lekiwi.py
+++ 1_Quellcode/main_lekiwi.py
@@ -14,7 +14,7 @@ ENABLE_MOVEMENT = True
 def main():
     robot = LeKiwiInterface(
         port="/dev/ttyACM0",
-        linear_speed=0.07
+        linear_speed=0.05
     )
 
     detector = ArucoDetector()
